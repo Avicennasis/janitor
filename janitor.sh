a1f50fbc9eec827094b1f3da09de29da925de9d2
Avic <Avicennasis@gmail.com>
2017-03-27 22:48:48 -0400

    Update janitor.sh
    fix missing ), double quotes to prevent globbing and word splitting.

--- janitor.sh
+++ janitor.sh
@@ -10,22 +10,22 @@ find /cygdrive/d/Mil-Main/TEMP/* -mtime +30 -exec du -bs {} \; | sed s/"\/cygdri
 awk '{x+=$0}END{print x}' /cygdrive/d/Mil-Main/TEMP/TEMPSIZE > /cygdrive/d/Mil-Main/TEMP/TEMPSIZETOTAL 
 
 #Make the directory for janitor logs if it doesn't exist yet
-if [ ! -d ${HOME}/logs/janitor ]; then
+if [ ! -d "${HOME}/logs/janitor" ]; then
 	echo "Janitor directory not found! Creating it now."
 	mkdir -p "${HOME}/logs/janitor"
 fi
 
 #And make a dummy file for math to avoid first-run errors
-if [ ! -f ${HOME}/logs/janitor/tempsizeinbytes.txt ]; then
+if [ ! -f "${HOME}/logs/janitor/tempsizeinbytes.txt" ]; then
 	echo "Janitor log file not found! Creating a dummy file real quick"
-	echo 0 > ${HOME}/logs/janitor/tempsizeinbytes.txt
+	echo 0 > "${HOME}/logs/janitor/tempsizeinbytes.txt"
 fi
 
 #Combine that total with our logs total
-cat /cygdrive/d/Mil-Main/TEMP/TEMPSIZETOTAL ${HOME}/logs/janitor/tempsizeinbytes.txt > /cygdrive/d/Mil-Main/TEMP/TEMPSIZEFINALTOTAL 
+cat /cygdrive/d/Mil-Main/TEMP/TEMPSIZETOTAL "${HOME}/logs/janitor/tempsizeinbytes.txt" > /cygdrive/d/Mil-Main/TEMP/TEMPSIZEFINALTOTAL 
 
 #Copy the complete total (logs + current operation) to the log file
-awk '{x+=$0}END{print x}' /cygdrive/d/Mil-Main/TEMP/TEMPSIZEFINALTOTAL > ${HOME}/logs/janitor/tempsizeinbytes.txt
+awk '{x+=$0}END{print x}' /cygdrive/d/Mil-Main/TEMP/TEMPSIZEFINALTOTAL > "${HOME}/logs/janitor/tempsizeinbytes.txt"
 
 #Remove our temp files
 rm /cygdrive/d/Mil-Main/TEMP/TEMPSIZE
@@ -33,7 +33,7 @@ rm /cygdrive/d/Mil-Main/TEMP/TEMPSIZETOTAL
 rm /cygdrive/d/Mil-Main/TEMP/TEMPSIZEFINALTOTAL
 
 #Make the directory for janitor logs if it doesn't exist yet
-if [ ! -d ${HOME}/logs/janitor/$(date -u +%Y/%m/ ]; then
+if [ ! -d "${HOME}/logs/janitor/$(date -u +%Y/%m/)" ]; then
 	echo "Janitor current year/month folder not found! Creating it now"
 	mkdir -p "${HOME}/logs/janitor/$(date -u +%Y/%m/)"
 fi
